0004ed81ee1f2102d4cd54faa3ac66bdb293cb88
IGOR\igorr <igor.rafaraber2@hotmail.com>
2026-03-25 18:39:55 -0700

    __Routes
    "some functions changes"

--- FlatFinder/src/app/app.routes.ts
+++ FlatFinder/src/app/app.routes.ts
@@ -1,20 +1,20 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: 'login', loadComponent: () => import('./features/auth/login/login.component').then(m => m.LoginComponent) },
-  { path: 'register', loadComponent: () => import('./features/auth/register/register.component').then(m => m.RegisterComponent) },
+  { path: 'login', loadComponent: () => import('./features/auth/login/login').then(m => m.Login) },
+  { path: 'register', loadComponent: () => import('./features/auth/register/register').then(m => m.Register) },
 
-  { path: '', loadComponent: () => import('./core/layouts/main-layout/main-layout.component').then(m => m.MainLayoutComponent),
+  { path: '', loadComponent: () => import('./core/layouts/main-layout/main-layout').then(m => m.MainLayout),
     children: [
-      { path: '', loadComponent: () => import('./features/home/flat-search/flat-search.component').then(m => m.FlatSearchComponent) },
-      { path: 'flats/new', loadComponent: () => import('./features/flats/flat-form/flat-form.component').then(m => m.FlatFormComponent) },
-      { path: 'flats/:id', loadComponent: () => import('./features/flats/flat-detail/flat-detail.component').then(m => m.FlatDetailComponent) },
-      { path: 'flats/:id/edit', loadComponent: () => import('./features/flats/flat-form/flat-form.component').then(m => m.FlatFormComponent) },
-      { path: 'my-flats', loadComponent: () => import('./features/flats/my-flats/my-flats.component').then(m => m.MyFlatsComponent) },
-      { path: 'favourites', loadComponent: () => import('./features/flats/favourites/favourites.component').then(m => m.FavouritesComponent) },
-      { path: 'profile', loadComponent: () => import('./features/profile/profile-view/profile-view.component').then(m => m.ProfileViewComponent) },
-      { path: 'profile/edit', loadComponent: () => import('./features/profile/profile-edit/profile-edit.component').then(m => m.ProfileEditComponent) },
-      { path: 'admin/users', loadComponent: () => import('./features/admin/users-list/users-list.component').then(m => m.UsersListComponent) },
+      { path: '', loadComponent: () => import('./features/home/flat-search/flat-search').then(m => m.FlatSearch) },
+      { path: 'flats/new', loadComponent: () => import('./features/flats/flat-form/flat-form').then(m => m) },
+      { path: 'flats/:id', loadComponent: () => import('./features/flats/flat-detail/flat-detail').then(m => m.FlatDetail) },
+      { path: 'flats/:id/edit', loadComponent: () => import('./features/flats/flat-form/flat-form').then(m => m.FlatForm) },
+      { path: 'my-flats', loadComponent: () => import('./features/flats/my-flats/my-flats').then(m => m.MyFlats) },
+      { path: 'favourites', loadComponent: () => import('./features/flats/favourites/favourites').then(m => m.Favourites) },
+      { path: 'profile', loadComponent: () => import('./features/profile/profile-view/profile-view').then(m => m.ProfileView) },
+      { path: 'profile/edit', loadComponent: () => import('./features/profile/profile-edit/profile-edit').then(m => m.ProfileEdit) },
+      { path: 'admin/users', loadComponent: () => import('./features/admin/users-list/users-list').then(m => m.UsersList) },
       { path: '**', redirectTo: '' }
     ]
   }
